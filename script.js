4a0bc0425fdd5bacf23aca3667bd47e0b0f280f9
Jondo <chet9880@gmail.com>
2022-06-24 16:33:27 -0400

    added pass length

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+var passlen;
+
+function generatePassword() {
+  passlength();
+}
+
+function passlength() {
+  do {
+    var valid = false;
+    passlen = prompt("Pick a password length between 8 and 128");
+    if (isNaN(passlen) === true) {
+      alert(passlen + " is not a number");
+       valid = false;
+    } else if (passlen === null) {
+      valid = true;
+      return;
+    }else if (((Number(passlen) < 8) || (Number(passlen) > 128))){
+      alert(passlen + " is not between 8 and 128");
+      valid = false;
+    }
+    else{
+      valid = true;
+    }
+  }
+  while (valid === false);
+}
